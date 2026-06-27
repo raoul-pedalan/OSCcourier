59c69eff25992aaf4f30a64046ea4f65f05259cc
Raoul <bern.pierre@free.fr>
2026-06-27 09:34:10 +0200

    Initial Commit

---
+++ OSCTimeline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OSCTimeline
+//
+//  Created by bernard pierre on 27/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OSCTimeline/OSCTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSCTimelineApp.swift
+//  OSCTimeline
+//
+//  Created by bernard pierre on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct OSCTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
